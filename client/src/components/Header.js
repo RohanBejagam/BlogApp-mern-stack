1e4e7012e6ab5e377fe6afedc45d6918e9e30e02
RohanBejagam <rohan041102@gmail.com>
2023-09-10 17:31:19 +0530

    login and register done frontend

---
+++ client/src/components/Header.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Button,
+  Typography,
+  Tabs,
+  Tab,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "../redux/store";
+import { useNavigate } from "react-router-dom";
+const Header = () => {
+  //global state
+  const isLogin = useSelector((state) => state.isLogin);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  console.log(isLogin);
+
+  //state
+  const [value, setvalue] = useState("");
+
+  //logout
+  const handlelogout = () => {
+    try {
+      dispatch(authActions.logout());
+      alert("Loggeout successful");
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    <>
+      <AppBar position="sticky">
+        <Toolbar>
+          <Typography variant="h4">My Blog App</Typography>
+          {isLogin && (
+            <Box display={"flex"} marginLeft={"auto"} marginRight={"auto"}>
+              <Tabs
+                textColor="inherit"
+                value={value}
+                onChange={(e, val) => {
+                  setvalue(val);
+                }}
+              >
+                <Tab label="Blogs" LinkComponent={Link} to="/blogs" />
+                <Tab label="My Blogs" LinkComponent={Link} to="/my-blogs" />
+              </Tabs>
+            </Box>
+          )}
+          <Box display={"flex"} marginLeft="auto">
+            {!isLogin && (
+              <>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/login"
+                >
+                  Login
+                </Button>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/register"
+                >
+                  Register
+                </Button>
+              </>
+            )}
+            {isLogin && (
+              <Button onClick={handlelogout} sx={{ margin: 1, color: "white" }}>
+                Logout
+              </Button>
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
+
+export default Header;
